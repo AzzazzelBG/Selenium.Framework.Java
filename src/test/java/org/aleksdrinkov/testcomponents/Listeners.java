d61c3f5c432187b9dc6ee3c2dce966b6ad475ecd
Alexander Drinkov <adrinkov@gmail.com>
2024-01-23 22:14:51 +0200

    Create xml report

--- src/test/java/org/aleksdrinkov/testcomponents/Listeners.java
+++ src/test/java/org/aleksdrinkov/testcomponents/Listeners.java
@@ -4,11 +4,14 @@ import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.Status;
 import org.aleksdrinkov.resources.ExtentReporterNG;
+import org.openqa.selenium.WebDriver;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
-public class Listeners implements ITestListener {
+import java.io.IOException;
+
+public class Listeners extends BaseTest implements ITestListener {
 
     ExtentTest test;
     ExtentReports extent = ExtentReporterNG.getReportObject();
@@ -27,6 +30,21 @@ public class Listeners implements ITestListener {
     public void onTestFailure(ITestResult result) {
         test.fail(result.getThrowable());
 
+        try {
+            driver = (WebDriver) result.getTestClass().getRealClass().getDeclaredField("driver").get(result.getInstance());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        String filePath = null;
+
+        try {
+            filePath = getScreenshot(result.getMethod().getMethodName(), driver);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        test.addScreenCaptureFromPath(filePath, result.getMethod().getMethodName());
     }
 
     @Override
@@ -51,6 +69,6 @@ public class Listeners implements ITestListener {
 
     @Override
     public void onFinish(ITestContext context) {
-        ITestListener.super.onFinish(context);
+        extent.flush();
     }
 }
--- src/test/java/org/aleksdrinkov/tests/StandAloneTestRefactored.java
+++ src/test/java/org/aleksdrinkov/tests/StandAloneTestRefactored.java
@@ -5,15 +5,11 @@ import org.aleksdrinkov.pageobjects.CheckoutPage;
 import org.aleksdrinkov.pageobjects.OrderPage;
 import org.aleksdrinkov.pageobjects.ProductCataloguePage;
 import org.aleksdrinkov.testcomponents.BaseTest;
-import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
@@ -52,16 +48,6 @@ public class StandAloneTestRefactored extends BaseTest {
         OrderPage orderPage = productCataloguePage.goToOrdersPage();
 
         Assert.assertTrue(orderPage.verifyOrderDisplay(productName));
-    }
-
-    public String getScreenshot(String testCaseName) throws IOException {
-
-        TakesScreenshot ts = (TakesScreenshot) driver;
-        File source = ts.getScreenshotAs(OutputType.FILE);
-        File file = new File(System.getProperty("user.dir") + "//reports//" + testCaseName + ".png");
-        FileUtils.copyFile(source, file);
-
-        return System.getProperty("user.dir") + "//reports//" + testCaseName + ".png";
     }
 
     @DataProvider
