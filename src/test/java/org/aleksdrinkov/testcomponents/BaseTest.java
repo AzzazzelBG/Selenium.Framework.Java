3300771d5e7092957eef8a3f7428562277a1b234
Alexander Drinkov <adrinkov@gmail.com>
2024-01-30 23:07:04 +0200

    Run tests with different browsers

--- src/test/java/org/aleksdrinkov/testcomponents/BaseTest.java
+++ src/test/java/org/aleksdrinkov/testcomponents/BaseTest.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -29,12 +30,19 @@ public class BaseTest {
 
         // If you want to use several browserst use these properties
         Properties prop = new Properties();
-        FileInputStream fis = new FileInputStream(new File( System.getProperty("user.dir") + "\\src\\main\\java\\org\\aleksdrinkov\\resources\\globaldata.properties"));
+        FileInputStream fis = new FileInputStream(new File( System.getProperty("user.dir") +
+                "\\src\\main\\java\\org\\aleksdrinkov\\resources\\globaldata.properties"));
         prop.load(fis);
-        String browserName = prop.getProperty("browser");
+        String browserName = System.getProperty("browser") != null ? System.getProperty("browser") : prop.getProperty("browser");
 
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+        if (browserName.equalsIgnoreCase("chrome")) {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+        } else if (browserName.equalsIgnoreCase("edge")) {
+            WebDriverManager.edgedriver().setup();
+            driver = new EdgeDriver();
+        }
+
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
 
@@ -75,6 +83,6 @@ public class BaseTest {
 
     @AfterMethod(alwaysRun = true)
     public void tearDown() {
-        driver.quit();
+        driver.close();
     }
 }
